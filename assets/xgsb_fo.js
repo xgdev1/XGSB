9e83ea105449d3a5ae7831f41b3bccf4a8499cd7
Xavier Govi <xavier@xgdev.fr>
2025-01-24 10:23:00 +0000

    Debug Menu

--- assets/xgsb_fo.js
+++ assets/xgsb_fo.js
@@ -1,11 +1,18 @@
 import './styles/xgsb_fo.scss';
 import $ from 'jquery';
 import 'bootstrap';
+import { Collapse } from 'bootstrap';
 
+var mainMenu;
 console.log('This log frontoffice');
 
 $(document).ready(function (){
+    mainMenu=new Collapse("#navbarEpro",{toggle:false});
+    mainMenu.hide();
+    $("#btnMenu").on("click",function(){
+       mainMenu.toggle();
+    });
     $("video").each(function() {
       $(this).get(0).play()
-  });
+    });
 })
